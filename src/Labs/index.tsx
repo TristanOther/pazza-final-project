19c97d1ca7e82111b1bf4f059cdc65fe718eac77
TristanOther <tristan.n.lyons@gmail.com>
2025-02-03 21:15:08 -0500

    Initial commit to assignment 2 branch.

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,6 +6,18 @@ import Lab3 from "./Lab3";
 export default function Labs() {
     return (
         <div id="wd-labs">
+            <h1>Info</h1>
+            <p>
+                I've placed this section here based on the assignment two instructions on Canvas, which 
+                said this information needed to be available as part of the labs page. As it was not all
+                included in lab 1 (the github link was there, but at the bottom of the page) I chose to 
+                include it here for ease of access.
+            </p>
+            <p>
+                Tristan Lyons<br />
+                CS4550 Section 4<br />
+                <a href="https://github.com/TristanOther/kambaz-react-web-app" id="wd-github">This project's GitHub repo.</a> (I was instructed by a TA to make this repo public after submission until grading is complete so the TAs can grade my work.) 
+            </p>
             <h1>Labs</h1>
             <TOC />
             <Routes>
