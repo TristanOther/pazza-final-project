40333316aaf33d03d2563b4500efe9fd7b70dd10
James Roth <roth.jame@northeastern.edu>
2025-04-07 14:57:30 -0400

    saving FF bar progress

--- src/Pazza/FileFolder/FileFolderNavigation.tsx
+++ src/Pazza/FileFolder/FileFolderNavigation.tsx
@@ -1,7 +1,34 @@
+import { Button } from "react-bootstrap";
+import { FaFolder } from "react-icons/fa";
+import { useState } from "react";
+
 export default function FileFolderNavigation() {
+
+    const [selectedFolder, setSelectedFolder] = useState("");
+
+    // Eventually this comes from backend
+    const folders = [
+        "Hw1",
+        "Hw2",
+        "Hw3",
+        "Hw4",
+        "Hw5",
+        "Project1",
+        "Project2",
+    ]
+
     return (
-        <div style={{ width: "100%", background: "LightBlue" }}>
-            File folder navigation bar.
+        <div className="d-flex" style={{ width: "100%", background: "LightGrey" }}>
+            {folders.map((folder) => (
+                <div>
+                    <FaFolder className="fs-5 ms-3" key={folder} />
+                    <Button type="button" variant="link" style={{ textDecoration: "none", color: "black" }}
+                        onClick={() => setSelectedFolder(folder)}>
+                        {folder}
+                        <span className="ms-1 pazza-unread-post-box">0</span>
+                    </Button>
+                </div>
+            ))}
         </div>
     );
 }
