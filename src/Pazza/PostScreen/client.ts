5e7d2f3c15debb138b4e9cae40a90c493efc71d7
TristanOther <tristan.n.lyons@gmail.com>
2025-04-17 01:33:28 -0400

    Completed intial working version of List of Posts Sidebar.

---
+++ src/Pazza/PostScreen/client.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+const axiosWithCredentials = axios.create({ withCredentials: true });
+const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+const POSTS_API = `${REMOTE_SERVER}/api/posts`;
+const COURSES_API = `${REMOTE_SERVER}/api/courses`;
+
+export const fetchPosts = async (cid: string) => {
+    const { data } = await axiosWithCredentials.get(`${COURSES_API}/${cid}/posts`);
+    return data;
+};
+
+export const createPost = async (post: any, cid: string) => {
+    const { data } = await axiosWithCredentials.post(`${COURSES_API}/${cid}/posts`, post);
+    return data;
+}
+
+export const deletePost = async (pid: string) => {
+    const { data } = await axiosWithCredentials.delete(`${POSTS_API}/${pid}`);
+    return data;
+}
+
+export const updatePost = async (post: any) => {
+    const { data } = await axiosWithCredentials.put(`${POSTS_API}/${post._id}`, post);
+    return data;
+}
+
